75a9ac4df2d386207f707449be064cc41a7927d8
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-16 11:38:36 +0800

    Started components for side panel.

--- src/components/ListedIssue.tsx
+++ src/components/ListedIssue.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import Span from './Span';
 import Label from './Label';
 import AppModel from '../models/AppModel';
@@ -9,18 +10,28 @@ type ListedIssueProps = {
   appStore: AppModel;
 };
 
-const Issue: React.FC<ListedIssueProps> = ({ text, labels, appStore }) => {
+const ListedIssue: React.FC<ListedIssueProps> = ({
+  text,
+  labels,
+  appStore,
+}) => {
   return (
     <>
       <Span text={text} className="listed-issue" />
-      {labels.map((label) => {
-        return (
-          <Label
-            text={label}
-            color={appStore.labelStore.getColorForLabel(label)}
-          />
-        );
-      })}
+      {appStore.labelStore.loading ? (
+        <Span className="label-loader" text="Loading labels..." />
+      ) : (
+        labels.map((label) => {
+          return (
+            <Label
+              text={label}
+              color={appStore.labelStore.getColorForLabel(label)}
+            />
+          );
+        })
+      )}
     </>
   );
 };
+
+export default observer(ListedIssue);
---
+++ src/containers/IssuesPanel.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import Span from '../components/Span';
+import AppModel from '../models/AppModel';
+import ListedIssue from '../components/ListedIssue';
+
+type IssuesPanelProps = {
+  appStore: AppModel;
+};
+
+const IssuesPanel: React.FC<IssuesPanelProps> = ({ appStore }) => {
+  return (
+    <>
+      {appStore.issueStore.loading ? (
+        <Span className="issue-loader" text="Loading issues..." />
+      ) : (
+        <>
+          {appStore.issueStore.issues.map((issue) => {
+            return (
+              <ListedIssue
+                text={issue.text}
+                labels={issue.labels}
+                appStore={appStore}
+              />
+            );
+          })}
+        </>
+      )}
+    </>
+  );
+};
+
+export default observer(IssuesPanel);
