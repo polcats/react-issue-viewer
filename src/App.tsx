ed4eb7c73f3128966de58fc76b0d6a6191484d62
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-16 09:14:59 +0800

    Initial retrieval of open issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Gitlab } from '@gitbeaker/browser';
+import { ProjectsBundle } from '@gitbeaker/browser';
 
-const api = new Gitlab({
+const api = new ProjectsBundle({
   host: 'https://gitlab.icannhas.com/',
   token: '2syhQoC3hVxWvVyZueHH',
 });
 
 const a = async () => {
-  let test = await api.Projects.search('WebRTC');
-  console.log(test);
+  let projectIssues = JSON.stringify(
+    await api.Issues.all({ projectId: 349, groupId: 120 }),
+  );
+  let data = await JSON.parse(projectIssues);
+  data = data.filter((item: typeof data[0]) => item.closed_at === null);
+
+  console.log(data);
 };
 a();
 
